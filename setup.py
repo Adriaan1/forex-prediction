5a83a65e8aa9fb6a5fb7a8ec92faf9ecc67a5267
Adriaan1 <adriaan.george@gmail.com>
2021-08-02 13:36:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   description = 'prediction of stock price or forex price',
   author = 'Nagi',
   author_email = 'komoootv@gmail.com',
-  license = "MIT,"
+  license = "MIT",
   packages=find_packages(['forex-prediction']),
   # packages = ['forex-prediction']
   install_requires = ['tensorflow', 'einops', 'yfinance', 'MetaTrader5', 'numpy', 'pandas', 'sklearn']
