d7be012eea91c998e7fa8b239ef829748c87f3ab
Adriaan1 <adriaan.george@gmail.com>
2021-08-02 12:15:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = "prediction",
-  version = 1.0,
+  version = "1.0",
   description = 'prediction of stock price or forex price',
   author = 'Nagi',
   author_email = 'komoootv@gmail.com',
